6f2d4f7495299756c4b882073d69839e1479402a
Stephen Pearson <spearson@sra.com.au>
2022-05-19 14:54:45 +0930

    click/right click and context menu/selection

--- src/debug/terrain.jsx
+++ src/debug/terrain.jsx
@@ -2,12 +2,18 @@ import React, {useState} from 'https://cdn.skypack.dev/react';
 
 function Terrain(props) {
 	
-	const click = ()=>{
-		props.cost+=1;
+	const click = (e)=>{
+		if(e.button==0){//left click
+			props.cost+=1;
+		}
+		if(e.button==2){//right click
+			props.cost-=1;
+		}
 		props.cellChange(props.idx,props.cost);
+		return false;
 	};
 	
-  return (<span onClick={click}>
+  return (<span onClick={click} style={{ userSelect: 'none'}} onContextMenu={(e)=>{click(e);e.preventDefault();return false;}}>
   {props.cost}
   </span>);
 }
